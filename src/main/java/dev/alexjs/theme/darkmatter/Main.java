7af6efc035fb52d489bd0693fd5c4873043d46e1
Alex Sweeney <ajs1998@gmail.com>
2023-09-27 13:11:42 -0500

    description

---
+++ src/main/java/dev/alexjs/theme/darkmatter/Main.java
@@ -0,0 +1,13 @@
+package dev.alexjs.theme.darkmatter;
+
+public class Main {
+
+    private static final int x = 0;
+
+    public static final String y = "Hello, world!";
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
