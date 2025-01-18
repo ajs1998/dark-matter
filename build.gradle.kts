3e2d1ad7a4f964a81474c4f6f6df8ec3680877bb
Alex Sweeney <ajs1998@gmail.com>
2025-01-17 20:47:29 -0600

    bump to 251

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ tasks {
     // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("243.*")
+        untilBuild.set("251.*")
     }
 
     signPlugin {
